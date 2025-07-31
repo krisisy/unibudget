d1db1301ffde99cb0151208170e98d2f8de7c4f1
CIITSHSDTP00\CIIT <94095237+xyvii@users.noreply.github.com>
2025-07-31 17:04:08 +0800

    fixing login issue

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -3,18 +3,23 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function POST(req) {
-  const { email, password } = await req.json()
+  try {
+    const { email, password } = await req.json()
 
-  const user = await prisma.user.findUnique({ where: { email } })
+    const user = await prisma.user.findUnique({ where: { email } })
 
-  if (!user) {
-    return NextResponse.json({ error: 'User does not exist' }, { status: 404 })
+    if (!user) {
+      return NextResponse.json({ error: 'User does not exist' }, { status: 401 })
+    }
+
+    if (user.password !== password) {
+      return NextResponse.json({ error: 'Incorrect password' }, { status: 401 })
+    }
+
+    cookies().set('user', user.email, { httpOnly: true })
+    return NextResponse.json({ success: true })
+  } catch (error) {
+    console.error('Login error:', error)
+    return NextResponse.json({ error: 'Internal Server Error'}, { status: 500})
   }
-
-  if (user.password !== password) {
-    return NextResponse.json({ error: 'Incorrect password' }, { status: 401 })
-  }
-
-  cookies().set('user', user.email, { httpOnly: true })
-  return NextResponse.json({ success: true })
 }
